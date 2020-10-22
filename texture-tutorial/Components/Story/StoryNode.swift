795d3a6df2428e95afc6ed9bf3a9da25c8f0a3ea
omrobbie <putu.roby@yahoo.com>
2020-10-22 11:38:33 +0800

    setup story node

--- texture-tutorial/Components/Story/StoryNode.swift
+++ texture-tutorial/Components/Story/StoryNode.swift
@@ -10,15 +10,44 @@ import AsyncDisplayKit
 
 class StoryNode: ASCollectionNode{
     let stories: [Story]
-    
+
     init(stories: [Story]) {
         self.stories = stories
-        
+
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         layout.minimumLineSpacing = 0
         layout.minimumInteritemSpacing = 0
-        
+
         super.init(frame: .zero, collectionViewLayout: layout, layoutFacilitator: nil)
+
+        style.width = ASDimension(unit: .fraction, value: 1)
+        style.height = ASDimension(unit: .points, value: 75)
+
+        delegate = self
+        dataSource = self
+
+        view.showsHorizontalScrollIndicator = false
+    }
+}
+
+extension StoryNode: ASCollectionDelegate, ASCollectionDataSource {
+    func numberOfSections(in collectionNode: ASCollectionNode) -> Int {
+        return 1
+    }
+
+    func collectionNode(_ collectionNode: ASCollectionNode, numberOfItemsInSection section: Int) -> Int {
+        return stories.count
+    }
+
+    func collectionNode(_ collectionNode: ASCollectionNode, nodeBlockForItemAt indexPath: IndexPath) -> ASCellNodeBlock {
+        guard stories.count > indexPath.row else {return { ASCellNode() }}
+        let story = stories[indexPath.row]
+
+        let cellNodeBlock = { () -> ASCellNode in
+            return StoryCell(story: story)
+        }
+
+        return cellNodeBlock
     }
 }
