98f7987ab17baea9cc2c527da20382ed0c81ca41
omrobbie <putu.roby@yahoo.com>
2020-10-22 11:17:32 +0800

    setup post cell node

--- texture-tutorial/Components/Post/PostCell.swift
+++ texture-tutorial/Components/Post/PostCell.swift
@@ -12,12 +12,44 @@ class PostCell: ASCellNode {
     let post: Post
     
     // MARK: - Nodes
+
+    let headerNode: HeaderNode
+    let postImage: ASImageNode
+    let actionNode: ActionNode
+    let postLikes: ASTextNode
+    let postDescription: ASTextNode
     
     init(post: Post) {
         self.post = post
+
+        headerNode = HeaderNode(post: post)
+
+        postImage = ASImageNode()
+        postImage.contentMode = .scaleAspectFit
+        postImage.style.width = ASDimension(unit: .fraction, value: 1)
+        postImage.image = post.image
+
+        actionNode = ActionNode()
+
+        postLikes = ASTextNode()
+        postLikes.attributedText = NSAttributedString.bold("\(post.likeCount) Likes")
+
+        postDescription = ASTextNode()
+        postDescription.maximumNumberOfLines = 0
+        postDescription.attributedText = NSAttributedString.normal(post.description)
         
         super.init()
         
         self.automaticallyManagesSubnodes = true
+    }
+
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let postImageWithRatio = ASRatioLayoutSpec(ratio: 1/1.5, child: postImage)
+        let postLikeWithPadding = ASInsetLayoutSpec(insets: UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8), child: postLikes)
+        let postDescriptionWithPadding = ASInsetLayoutSpec(insets: UIEdgeInsets(top: 4, left: 8, bottom: 0, right: 8), child: postDescription)
+
+        let mainStack = ASStackLayoutSpec(direction: .vertical, spacing: 0, justifyContent: .start, alignItems: .start, children: [headerNode, postImageWithRatio, actionNode, postLikeWithPadding, postDescriptionWithPadding])
+
+        return mainStack
     }
 }
